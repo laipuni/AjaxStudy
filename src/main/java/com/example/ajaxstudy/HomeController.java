c6f7ae0ba151b3e4f3fe7c2c51e1b392fa6e6a1a
laipuni <2tgb02023@gmail.com>
2024-06-23 21:26:56 +0900

    feat : impl /ajaxTest by get

--- src/main/java/com/example/ajaxstudy/HomeController.java
+++ src/main/java/com/example/ajaxstudy/HomeController.java
@@ -1,14 +1,35 @@
-//package com.example.ajaxstudy;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class HomeController {
-//
-//    @GetMapping("/")
-//    public String home(){
-//        return "index";
-//    }
-//
-//}
+package com.example.ajaxstudy;
+
+import jakarta.annotation.Nullable;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(@Nullable @RequestParam("name")String name, Model model){
+        model.addAttribute("name",name);
+        return "index";
+    }
+
+    @GetMapping("/ajaxTest")
+    public String ajaxTest(){
+        return "ajaxTest";
+    }
+
+    @GetMapping("/ajaxTest/click")
+    @ResponseBody
+    public List<String> ajaxClickTest(){
+        log.info("ajaxClickTest");
+        return List.of("a","b","c","d");
+    }
+
+}
