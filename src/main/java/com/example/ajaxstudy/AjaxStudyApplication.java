5b795f55f85bcf59a928451ec430f22b25000190
laipuni <2tgb02023@gmail.com>
2024-06-23 11:55:29 +0900

    [back] feat : upload main

---
+++ src/main/java/com/example/ajaxstudy/AjaxStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajaxstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjaxStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjaxStudyApplication.class, args);
+    }
+
+}
