951293091ccbb8d069ea4104aec921c96bfe3bd1
laipuni <2tgb02023@gmail.com>
2024-06-29 20:01:11 +0900

    feat : 답글들의 댓글 Id로 최신순으로 조회하는 기능 개발
    findAllByParentIdDesc : 답글들의 댓글 Id인 parentId parameter로 최신순으로 조회

--- src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
+++ src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
@@ -1,7 +1,13 @@
 package com.example.ajaxstudy.domain.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
+    @Query(value = "select * from Comment c where c.parent =: parentId order by c.id desc",nativeQuery = true)
+    public List<Comment> findAllByParentIdDesc(@Param("parentId") Long parentId);
 
 }
