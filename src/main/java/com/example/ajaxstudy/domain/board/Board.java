8f6ada529a7d7ea27574c2f97fd5f6fc71111d4e
laipuni <2tgb02023@gmail.com>
2024-06-29 16:22:31 +0900

    feat : 게시물 정보를 저장하는 Board Entity개발
    id : DB에 식별될 Id값
    title : 게시물의 제목
    contents : 게시물의 내용
    writer : 게시물 작성자 이름
    heartNum : 좋아요 개수
    hearts : Heart Entity 외래 키
    comments : Comment Entity 외래 키
    of() : Board Entity를 생성하는 static 메소드
    addHeart() : Heart Entity를 추가하는 메소드
    addComment() : Comment Entity를 추가하는 메소드
    incrementHeartNum() : heartNum를 증가하는 메소드

---
+++ src/main/java/com/example/ajaxstudy/domain/board/Board.java
@@ -0,0 +1,90 @@
+package com.example.ajaxstudy.domain.board;
+
+import com.example.ajaxstudy.domain.comment.Comment;
+import com.example.ajaxstudy.domain.heart.Heart;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+public class Board {
+    public static final int HEART_INIT = 0;
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false)
+    private int heartNum;
+
+    @OneToMany(mappedBy = "board")
+    private List<Heart> hearts = new ArrayList<>();
+
+    @BatchSize(size = 100)
+    @OneToMany(mappedBy = "board")
+    private List<Comment> comments = new ArrayList<>();
+
+    @Builder
+    private Board(final String title,final String writer,final String contents, final int heartNum) {
+        this.title = title;
+        this.writer = writer;
+        this.contents = contents;
+        this.heartNum = heartNum;
+    }
+
+    public static Board of(final String title,final String writer,final String contents){
+        return Board.builder()
+                .title(title)
+                .writer(writer)
+                .contents(contents)
+                .heartNum(HEART_INIT)
+                .build();
+    }
+
+    public void addHeart(final Heart heart) {
+        hearts.add(heart);
+        incrementHeartNum();
+    }
+
+    public void addComment(final Comment comment){
+        comments.add(comment);
+    }
+
+    public void incrementHeartNum() {
+        this.heartNum += 1;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public int getHeartNum() {
+        return heartNum;
+    }
+}
