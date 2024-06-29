d7326e08b0a039d9af945d179b5d1030ea2953f1
laipuni <2tgb02023@gmail.com>
2024-06-29 19:03:00 +0900

    feat : 게시글 좋아요 API개발
    addHeart() : 게시글 좋아요를 눌렀을 때 좋아요를 추가할 Api

---
+++ src/main/java/com/example/ajaxstudy/domain/heart/Api/HeartController.java
@@ -0,0 +1,27 @@
+package com.example.ajaxstudy.domain.heart.Api;
+
+import com.example.ajaxstudy.domain.heart.HeartService;
+import com.example.ajaxstudy.domain.heart.request.HeartAddRequest;
+import com.example.ajaxstudy.domain.ApiResponse;
+import com.example.ajaxstudy.domain.heart.response.HeartAddResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HeartController {
+
+    private final HeartService heartService;
+
+    @PostMapping("/heart")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<Object> addHeart(@Valid @RequestBody HeartAddRequest request){
+        HeartAddResponse heartAddResponse = heartService.addHeart(request);
+        return ApiResponse.of(HttpStatus.CREATED,heartAddResponse);
+    }
+}
