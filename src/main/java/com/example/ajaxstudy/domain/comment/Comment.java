5269ef7bc9e0ffb7319d30504757fb5029086ae9
laipuni <2tgb02023@gmail.com>
2024-06-29 15:21:52 +0900

    feat : impl Comment Entity

---
+++ src/main/java/com/example/ajaxstudy/domain/comment/Comment.java
@@ -0,0 +1,73 @@
+package com.example.ajaxstudy.domain.comment;
+
+import com.example.ajaxstudy.domain.board.Board;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false)
+    private String contents;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade =  CascadeType.ALL)
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Comment parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Comment> childs = new ArrayList<>();
+
+    @Builder
+    private Comment(final Board board,final String contents, final String writer) {
+        this.board = board;
+        this.contents = contents;
+        this.writer = writer;
+    }
+
+    public static Comment of(final Board board,final String contents,final String writer){
+        Comment comment = Comment.builder()
+                .board(board)
+                .writer(writer)
+                .contents(contents)
+                .build();
+        board.addComment(comment);
+        return comment;
+    }
+
+    public static Comment of(final Board board, final String contents, final String writer , final Comment parent){
+        Comment comment = Comment.builder()
+                .board(board)
+                .writer(writer)
+                .contents(contents)
+                .build();
+        comment.changeParentComment(parent);
+        parent.addChildComment(comment);
+        board.addComment(comment);
+        return comment;
+    }
+
+    public void changeParentComment(Comment parent){
+        this.parent = parent;
+    }
+    public void addChildComment(Comment child){
+        childs.add(child);
+    }
+}
