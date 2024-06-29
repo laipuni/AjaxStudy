7a1cf01d2f911ce7a8fa77eadd3f0153e9f5b3b1
laipuni <2tgb02023@gmail.com>
2024-06-29 15:58:56 +0900

    test : CommentController 테스트 코드 작성
    addComment() : "/comment" 해당 url로 post Method가 올 경우,
    boardId,writer,contents값들을 json으로 제대로 받는지 테스트
    addCommentWithNoExistBoard() : "/comment"로 존재하지 않는 boardId 값을 json으로 받을경우 에러가 발생하는지 테스트
    addCommentWithBlankContents() : "/comment"로 " ","",NULL값이 contents값으로 넘어올 경우 에러가 발생하는지 테스트
    addCommentWithBlankWriter() : "/comment"로 writer값이 " ","",NULL으로 넘어올 경우 에러가 발생하는지 테스트
    addReply("/comment/reply") : "/comment/reply" 해당 url로 post Method가 올 경우,boardId,writer,commentId,contents값들을 json으로 제대로 받는지 테스트
    addReplyWithNoExistBoard() : "/comment/reply"로 존재하지 않는 boardId 값을 json으로 받을경우 에러가 발생하는지 테스트
    addReplyWithBlankContents() : "/comment/reply"로 contents로 " ","",NULL넘어올 경우 에러가 발생하는지 테스트
    addReplyWithBlankWriter() : "/comment/reply"로 writer값이 " ","",NULL로 넘어올 경우 에러가 발생하는지 테스트
     addReplyWithNoExistComment() : "/comment/reply"로 존재하지 않는 댓글의 Id가 넘어올 경우 에러가 발생하는지 테스트

---
+++ src/test/java/com/example/ajaxstudy/domain/comment/Api/CommentControllerTest.java
@@ -0,0 +1,271 @@
+package com.example.ajaxstudy.domain.comment.Api;
+
+import com.example.ajaxstudy.domain.comment.CommentService;
+import com.example.ajaxstudy.domain.comment.request.CommentAddRequest;
+import com.example.ajaxstudy.domain.comment.request.CommentReplyRequest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = CommentController.class)
+class CommentControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockBean
+    protected CommentService commentService;
+
+    @DisplayName("해당 게시글에 댓글을 작성할 경우, 댓글을 등록하고 결과를 반환한다.")
+    @Test
+    void addComment() throws Exception {
+        //given
+        CommentAddRequest request = CommentAddRequest.builder()
+                .boardId(0L)
+                .writer("라이푸니")
+                .contents("내용")
+                .build();
+        String data = objectMapper.writeValueAsString(request);
+
+        //when//then
+        mockMvc.perform(
+                MockMvcRequestBuilders.post("/comment")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(data)
+        )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.status").value("CREATED"))
+                .andExpect(jsonPath("$.code").value(201))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("존재하지 않는 게시글에 댓글을 작성할 경우, 에러가 발생한다.")
+    @Test
+    void addCommentWithNoExistBoard() throws Exception {
+        //given
+        CommentAddRequest request = CommentAddRequest.builder()
+                .boardId(0L)
+                .writer("라이푸니")
+                .contents("내용")
+                .build();
+        String data = objectMapper.writeValueAsString(request);
+
+        Mockito.when(commentService.record(Mockito.any(CommentAddRequest.class)))
+                        .thenThrow(new IllegalArgumentException("해당 게시물은 존재하지 않습니다."));
+
+        //when//then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/comment")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("해당 게시물은 존재하지 않습니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("빈 댓글을 작성할 경우, 에러가 발생한다.")
+    @Test
+    void addCommentWithBlankContents() throws Exception {
+        //given
+        CommentAddRequest request = CommentAddRequest.builder()
+                .boardId(0L)
+                .writer("라이푸니")
+                .contents(" ")
+                .build();
+        String data = objectMapper.writeValueAsString(request);
+
+        //when//then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/comment")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("댓글을 입력해주세요."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("작성자이름없이 댓글을 작성할 경우, 에러가 발생한다.")
+    @Test
+    void addCommentWithBlankWriter() throws Exception {
+        //given
+        CommentAddRequest request = CommentAddRequest.builder()
+                .boardId(0L)
+                .writer(" ")
+                .contents("안녕하세요")
+                .build();
+        String data = objectMapper.writeValueAsString(request);
+
+        //when//then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/comment")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("작성자의 이름을 입력해주세요"))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("해당 댓글에 답글을 작성할 경우, 답글을 등록하고 결과를 반환한다.")
+    @Test
+    void addReply() throws Exception {
+        //given
+        CommentReplyRequest request = CommentReplyRequest.builder()
+                .boardId(1L)
+                .commentId(1L)
+                .writer("라이푸니")
+                .contents("내용")
+                .build();
+        String data = objectMapper.writeValueAsString(request);
+
+        //when//then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/comment/reply")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.status").value("CREATED"))
+                .andExpect(jsonPath("$.code").value(201))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("존재하지 않는 게시글에 답글을 작성할 경우, 에러가 발생한다.")
+    @Test
+    void addReplyWithNoExistBoard() throws Exception {
+        //given
+        CommentReplyRequest request = CommentReplyRequest.builder()
+                .boardId(1L)
+                .commentId(1L)
+                .writer("라이푸니")
+                .contents("내용")
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        Mockito.when(commentService.reply(Mockito.any(CommentReplyRequest.class)))
+                .thenThrow(new IllegalArgumentException("해당 게시물은 존재하지 않습니다."));
+
+        //when//then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/comment/reply")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("해당 게시물은 존재하지 않습니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("빈 답글을 작성할 경우, 에러가 발생한다.")
+    @Test
+    void addReplyWithBlankContents() throws Exception {
+        //given
+        CommentReplyRequest request = CommentReplyRequest.builder()
+                .boardId(1L)
+                .commentId(1L)
+                .writer("라이푸니")
+                .contents(" ")
+                .build();
+
+        String data = objectMapper.writeValueAsString(request);
+
+        //when//then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/comment")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("댓글을 입력해주세요."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("작성자이름없이 답글을 작성할 경우, 에러가 발생한다.")
+    @Test
+    void addReplyWithBlankWriter() throws Exception {
+        //given
+        CommentReplyRequest request = CommentReplyRequest.builder()
+                .boardId(1L)
+                .commentId(1L)
+                .writer(" ")
+                .contents("내용")
+                .build();
+        String data = objectMapper.writeValueAsString(request);
+
+        //when//then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/comment/reply")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("작성자의 이름을 입력해주세요"))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("존재하지 않는 댓글에 답글을 작성할 경우, 에러가 발생한다.")
+    @Test
+    void addReplyWithNoExistComment() throws Exception {
+        //given
+        CommentReplyRequest request = CommentReplyRequest.builder()
+                .boardId(1L)
+                .commentId(1L)
+                .writer("라이푸니")
+                .contents("내용")
+                .build();
+        String data = objectMapper.writeValueAsString(request);
+
+        Mockito.when(commentService.reply(Mockito.any(CommentReplyRequest.class)))
+                .thenThrow(new IllegalArgumentException("해당 댓글은 존재하지 않습니다."));
+
+        //when//then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.post("/comment/reply")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(data)
+                )
+                .andDo(print())
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("해당 댓글은 존재하지 않습니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+}
