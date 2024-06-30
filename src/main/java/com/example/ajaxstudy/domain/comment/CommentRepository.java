932e6b5be09c3ccaf13c64a88c709f3bd852688c
laipuni <2tgb02023@gmail.com>
2024-06-30 09:48:00 +0900

    bugfix : 답글 조회기능 Native Query가 아닌 Jpql로 바꾸다
    Native Query로 작성시 board.parent.id를 인식못하는 예외가 발생해서 Jpql로 변환했다.

--- src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
+++ src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
-    @Query(value = "select * from Comment c where c.parent =: parentId order by c.id desc",nativeQuery = true)
+    @Query(value = "select c from Comment as c where c.parent.id =:parentId order by c.id desc")
     public List<Comment> findAllByParentIdDesc(@Param("parentId") Long parentId);
 
 }
