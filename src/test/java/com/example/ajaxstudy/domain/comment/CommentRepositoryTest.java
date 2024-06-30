0d76998d3cc1aaa06afa9d03eb0c31712c6daaf1
laipuni <2tgb02023@gmail.com>
2024-06-30 21:25:33 +0900

    test : 게시판에 댓글 조회기능 개발 테스트
    findAllByNullDesc() : 게시판 1, 댓글1, 답글 1을 DB에 저장하고 댓글만 조회하는지 테스트

--- src/test/java/com/example/ajaxstudy/domain/comment/CommentRepositoryTest.java
+++ src/test/java/com/example/ajaxstudy/domain/comment/CommentRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.ajaxstudy.domain.comment;
 
 import com.example.ajaxstudy.domain.board.Board;
 import com.example.ajaxstudy.domain.board.BoardRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import static org.assertj.core.api.Assertions.tuple;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class CommentRepositoryTest {
 
     @Autowired
@@ -24,14 +26,7 @@ class CommentRepositoryTest {
     @Autowired
     protected BoardRepository boardRepository;
 
-    @BeforeEach
-    void tearUp(){
-        commentRepository.deleteAllInBatch();
-        boardRepository.deleteAllInBatch();
-    }
-
     @DisplayName("조회할 답글들의 댓글의 Id를 받아 답글들을 조회한다.")
-    @Transactional
     @Test
     void findAllByParentIdDesc(){
         //given
@@ -70,6 +65,44 @@ class CommentRepositoryTest {
                         tuple("자식","자식")
                 );
 
+    }
+
+    @DisplayName("parent Comment가 Null인 해당 게시물에 Comment를 최신순으로 조회한다.")
+    @Test
+    void findAllByNullDesc(){
+        //given
+        Board board = Board.builder()
+                .writer("작성자")
+                .title("제목")
+                .heartNum(0)
+                .contents("내용")
+                .build();
+        boardRepository.save(board);
+        Comment parent = Comment.builder()
+                .writer("부모")
+                .contents("부모!")
+                .board(board)
+                .build();
+        commentRepository.save(parent);
+        String writer = "자식";
+        String contents = "자식";
+        Comment child = Comment.builder()
+                .writer(writer)
+                .contents(contents)
+                .board(board)
+                .build();
+
+        child.changeParentComment(parent);
+        commentRepository.save(child);
+        //when
+        List<Comment> comments = commentRepository.findAllByBoardIdAndNullDesc(board.getId());
+
+        //then
+        Assertions.assertThat(comments).hasSize(1)
+                .extracting("writer","contents")
+                .containsExactlyInAnyOrder(
+                        tuple("부모","부모!")
+                );
     }
 
 }
