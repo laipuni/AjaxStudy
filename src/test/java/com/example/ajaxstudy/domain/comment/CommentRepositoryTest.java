ee3f6685329e60738307b2ba289acac8cdb0964e
laipuni <2tgb02023@gmail.com>
2024-06-30 09:46:05 +0900

    refactor : 답글 조회 기능테스트 코드 CommentServiceTest에서 CommentRepositoryTest로 옮김
    답글 조회기능은 Repository계층에서 구현된 기능으로 CommentServiceTest가 아닌 CommentRepositoryTest가 적합하다고 생각해서 옮겼다.

---
+++ src/test/java/com/example/ajaxstudy/domain/comment/CommentRepositoryTest.java
@@ -0,0 +1,81 @@
+package com.example.ajaxstudy.domain.comment;
+
+import com.example.ajaxstudy.domain.board.Board;
+import com.example.ajaxstudy.domain.board.BoardRepository;
+import com.example.ajaxstudy.domain.comment.request.CommentChildRequest;
+import com.example.ajaxstudy.domain.comment.response.CommentChildResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CommentRepositoryTest {
+
+    @Autowired
+    protected CommentRepository commentRepository;
+
+    @Autowired
+    protected BoardRepository boardRepository;
+
+    @BeforeEach
+    void tearUp(){
+        commentRepository.deleteAllInBatch();
+        boardRepository.deleteAllInBatch();
+    }
+
+    @DisplayName("조회할 답글들의 댓글의 Id를 받아 답글들을 조회한다.")
+    @Transactional
+    @Test
+    void findAllByParentIdDesc(){
+        //given
+        Board board = Board.builder()
+                .writer("작성자")
+                .title("제목")
+                .heartNum(0)
+                .contents("내용")
+                .build();
+        boardRepository.save(board);
+        Comment parent = Comment.builder()
+                .writer("부모")
+                .contents("부모!")
+                .board(board)
+                .build();
+        commentRepository.save(parent);
+        
+        String writer = "자식";
+        String contents = "자식";
+        Comment child = Comment.builder()
+                .writer(writer)
+                .contents(contents)
+                .board(board)
+                .build();
+
+        child.changeParentComment(parent);
+        commentRepository.save(child);
+
+        CommentChildRequest request = CommentChildRequest.builder()
+                .commentId(board.getId())
+                .build();
+
+        //when
+        List<Comment> childs = commentRepository.findAllByParentIdDesc(board.getId());
+
+        //then
+        assertThat(childs).hasSize(1)
+                .extracting("writer","contents")
+                .containsExactlyInAnyOrder(
+                        tuple("자식","자식")
+                );
+
+    }
+
+}
