6eca3b60d5aed9535d3644e9a2e798146fca1f22
laipuni <2tgb02023@gmail.com>
2024-07-01 09:19:09 +0900

    feat : 게시글 상세조회 Api에 게시글 조회기능 추가
    boardDetail() : "/boards/{boardId}"로 게시물을 상세조회할 때, 게시글 댓글 조회도 추가

--- src/main/java/com/example/ajaxstudy/domain/board/Api/BoardController.java
+++ src/main/java/com/example/ajaxstudy/domain/board/Api/BoardController.java
@@ -3,6 +3,8 @@ package com.example.ajaxstudy.domain.board.Api;
 import com.example.ajaxstudy.domain.board.BoardService;
 import com.example.ajaxstudy.domain.board.response.BoardDetailResponse;
 import com.example.ajaxstudy.domain.board.response.BoardListResponse;
+import com.example.ajaxstudy.domain.comment.CommentService;
+import com.example.ajaxstudy.domain.comment.response.CommentBoardResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,6 +18,7 @@ import java.util.List;
 public class BoardController {
 
     private final BoardService boardService;
+    private final CommentService commentService;
 
     @GetMapping("/boards")
     public String boards(Model model){
@@ -27,7 +30,9 @@ public class BoardController {
     @GetMapping("/boards/{boardId}")
     public String boardDetail(@PathVariable("boardId")Long boardId, Model model){
         BoardDetailResponse board = boardService.findById(boardId);
+        List<CommentBoardResponse> comments = commentService.findAllByBoardIdAndNullDesc(boardId);
         model.addAttribute("board",board);
+        model.addAttribute("comments",comments);
         return "board/boardDetail";
     }
 
