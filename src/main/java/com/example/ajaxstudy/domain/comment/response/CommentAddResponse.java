96ed206ee27d0509d3b8157f58fac153e007c53a
laipuni <2tgb02023@gmail.com>
2024-07-03 20:28:31 +0900

    feat : 게시글 댓글 추가 응답 Dto 데이터 추가
    commentId : 등록된 댓글의 식별키
    writer : 등록된 댓글의 작성자

--- src/main/java/com/example/ajaxstudy/domain/comment/response/CommentAddResponse.java
+++ src/main/java/com/example/ajaxstudy/domain/comment/response/CommentAddResponse.java
@@ -7,16 +7,22 @@ import lombok.Getter;
 @Getter
 public class CommentAddResponse {
 
+    private Long commentId;
     private String content;
+    private String writer;
 
     @Builder
-    private CommentAddResponse(final String content) {
+    private CommentAddResponse(final Long commentId, final String content, final String writer) {
+        this.commentId = commentId;
         this.content = content;
+        this.writer = writer;
     }
 
     public static CommentAddResponse of(Comment comment){
         return CommentAddResponse.builder()
+                .commentId(comment.getId())
                 .content(comment.getContents())
+                .writer(comment.getWriter())
                 .build();
     }
 }
