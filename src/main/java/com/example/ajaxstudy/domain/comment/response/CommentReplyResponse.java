fee4274272b7c1a0db402f7df8defb1cae13e247
laipuni <2tgb02023@gmail.com>
2024-07-03 20:29:17 +0900

    feat : 답글 추가 응답 Dto 데이터 추가
    commentId : 등록된 답글의 식별키
    writer : 등록된 답글의 작성자

--- src/main/java/com/example/ajaxstudy/domain/comment/response/CommentReplyResponse.java
+++ src/main/java/com/example/ajaxstudy/domain/comment/response/CommentReplyResponse.java
@@ -7,17 +7,22 @@ import lombok.Getter;
 @Getter
 public class CommentReplyResponse {
 
-
+    private Long commentId;
     private String content;
+    private String writer;
 
     @Builder
-    private CommentReplyResponse(final String content) {
+    private CommentReplyResponse(final Long commentId, final String content, final String writer) {
+        this.commentId = commentId;
         this.content = content;
+        this.writer = writer;
     }
 
     public static CommentReplyResponse of(Comment comment){
         return CommentReplyResponse.builder()
+                .commentId(comment.getId())
                 .content(comment.getContents())
+                .writer(comment.getWriter())
                 .build();
     }
 
