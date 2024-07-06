d1b7de108798ef1b700b63b7baf588435caf76a3
laipuni <2tgb02023@gmail.com>
2024-07-06 10:15:28 +0900

    feat : service 계층에서 Spring Data Jpa를 사용하지 않기 위한 PageAbleFactory개발
    Service계층을 최대한 순수 java코드만 사용하기 위해 PageAbleFactory로 Spring Data Jpa 기술을 분리시킴

---
+++ src/main/java/com/example/ajaxstudy/domain/PageAbleFactory.java
@@ -0,0 +1,19 @@
+package com.example.ajaxstudy.domain;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
+import static org.springframework.data.domain.Sort.by;
+
+public class PageAbleFactory {
+    public static final Direction DESC = Direction.DESC;
+    public static final Direction ASC = Direction.ASC;
+
+    public static Pageable create(int page, int pageSize, String sort, Direction direction){
+        Sort.Order order = new Sort.Order(direction,sort);
+        return PageRequest.of(page,pageSize,by(order));
+    }
+
+}
