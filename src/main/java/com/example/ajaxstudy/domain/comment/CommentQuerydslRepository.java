fe649f3fa19fc4e68ea83ea826598b8d5069cb92
laipuni <2tgb02023@gmail.com>
2024-07-06 21:31:34 +0900

    feat : QueryDsl를 이용한 답글 최신순 pagenation 조회 기능 개발
    findAllByParentIdDesc(commentId, pageable) : 부모 댓글의 commentId, page, pageSize(Page당 사이즈), sort가 담겨있는 Pageable를 통해 답글을 조회한다. Entity가 아닌 CommentChildResponse Dto로 원하는 coulmn만 조회한다. page당 size+1만큼 조회하는 이유는 그 다음이 존재하는지 확인할 수 있기 때문이다.

---
+++ src/main/java/com/example/ajaxstudy/domain/comment/CommentQuerydslRepository.java
@@ -0,0 +1,46 @@
+package com.example.ajaxstudy.domain.comment;
+
+import com.example.ajaxstudy.domain.comment.response.CommentChildResponse;
+import com.example.ajaxstudy.domain.comment.response.QCommentChildResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+import static com.example.ajaxstudy.domain.comment.QComment.comment;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentQuerydslRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public Slice<CommentChildResponse> findAllByParentIdDesc(Long commentId, Pageable pageable){
+        List<CommentChildResponse> content = queryFactory.select(
+                        new QCommentChildResponse(
+                                comment.id,
+                                comment.writer,
+                                comment.contents,
+                                comment.childs.size()
+                        )
+                )
+                .from(comment)
+                .where(comment.parent.id.eq(commentId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .orderBy(comment.id.desc())
+                .fetch();
+
+        return new SliceImpl<>(content,pageable,hasNext(content.size(), pageable));
+    }
+
+    private boolean hasNext(final int contentSize,Pageable pageable) {
+        return contentSize > pageable.getPageSize();
+    }
+}
