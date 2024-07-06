cb9b7cae40433335b37a7f9fc757870ed62aeb3b
laipuni <2tgb02023@gmail.com>
2024-07-06 22:08:50 +0900

    feat :  CommentRepository 답글 조회 기능 제거
    답글 조회기능은 Querydsl을 이용한 CommentQuerydslRepository에 구현했기 때문에 해당 CommentRepository에 답글 조회기능을 사용하지 않기 때문에 제거

--- src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
+++ src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
@@ -1,8 +1,5 @@
 package com.example.ajaxstudy.domain.comment;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,9 +7,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long>{
-    @Query(value = "select c from Comment as c where c.parent.id =:parentId")
-    public Slice<Comment> findAllByParentIdDesc(@Param("parentId") Long parentId, Pageable pageable);
-
     @Query(value = "select c from Comment as c where c.parent is null and c.board.id =:boardId order by c.id desc")
     public List<Comment> findAllByBoardIdAndNullDesc(@Param("boardId")Long boardId);
 
