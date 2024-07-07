04ba2604f791876982b80de2aea1f46d0cf50b7b
laipuni <2tgb02023@gmail.com>
2024-07-07 20:53:08 +0900

    feat : 게시물 댓글 조회 응답 dto 개발
    size :현재 dto에 담긴 comments들의 개수
    page : 현재 pagination에서 page 값
    hasNext : 아직 다음에 조회할 데이터가 남아있는지
    comments : 조회한 댓글들의 정보가 담긴 dto List

---
+++ src/main/java/com/example/ajaxstudy/domain/comment/response/CommentBoardListResponse.java
@@ -0,0 +1,36 @@
+package com.example.ajaxstudy.domain.comment.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentBoardListResponse {
+
+    private int size;
+    private int page;
+    private boolean hasNext;
+    private List<CommentBoardResponse> comments;
+
+    @Builder
+    private CommentBoardListResponse(final int size, final int page, final boolean hasNext, final List<CommentBoardResponse> comments) {
+        this.size = size;
+        this.page = page;
+        this.hasNext = hasNext;
+        this.comments = comments;
+    }
+
+    public static CommentBoardListResponse of(Slice<CommentBoardResponse> contents){
+        return CommentBoardListResponse.builder()
+                .size(contents.getSize())
+                .page(contents.getNumber())
+                .hasNext(contents.hasNext())
+                .comments(contents.getContent())
+                .build();
+    }
+}
