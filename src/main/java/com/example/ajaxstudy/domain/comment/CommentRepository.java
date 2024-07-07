7e9fecef1b8b95968a5bd636aee14fde561c33b6
laipuni <2tgb02023@gmail.com>
2024-07-07 20:44:46 +0900

    feat : CommentRepository에 게시글 댓글 조회 기능 제거
    Quertdsl을 이용한 게시글 댓글 최신순 조회기능을 CommentQuerydsl에서 개발했기 떄문에 제거

--- src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
+++ src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
@@ -1,13 +1,7 @@
 package com.example.ajaxstudy.domain.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long>{
-    @Query(value = "select c from Comment as c where c.parent is null and c.board.id =:boardId order by c.id desc")
-    public List<Comment> findAllByBoardIdAndNullDesc(@Param("boardId")Long boardId);
 
 }
