97394d58e6a173d432bb190edd69aa3512c1f744
laipuni <2tgb02023@gmail.com>
2024-07-07 20:39:52 +0900

    feat : querydsl을 이용한 게시글 댓글조회 기능 개발
    findAllByBoardIdAndNullDesc(boardId, Pageable) :
    Entity가 아닌 dto조회, pageSize + 1개 만큼 해당Id의 게시물에 댓글들을 최신순으로 조회한다. pageSize + 1개만큼 조회하는 이유는 해당 page 다음 데이터가 존재하는지 확인하기 위해서이다.

--- src/main/java/com/example/ajaxstudy/domain/comment/CommentQuerydslRepository.java
+++ src/main/java/com/example/ajaxstudy/domain/comment/CommentQuerydslRepository.java
@@ -1,17 +1,17 @@
 package com.example.ajaxstudy.domain.comment;
 
+import com.example.ajaxstudy.domain.comment.response.CommentBoardResponse;
 import com.example.ajaxstudy.domain.comment.response.CommentChildResponse;
+import com.example.ajaxstudy.domain.comment.response.QCommentBoardResponse;
 import com.example.ajaxstudy.domain.comment.response.QCommentChildResponse;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
-
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
 
 import static com.example.ajaxstudy.domain.comment.QComment.comment;
 
@@ -32,6 +32,29 @@ public class CommentQuerydslRepository {
                 )
                 .from(comment)
                 .where(comment.parent.id.eq(commentId))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize() + 1)
+                .orderBy(comment.id.desc())
+                .fetch();
+
+        return new SliceImpl<>(content,pageable,hasNext(content.size(), pageable));
+    }
+
+    public Slice<CommentBoardResponse> findAllByBoardIdAndNullDesc(Long boardId, Pageable pageable){
+
+        List<CommentBoardResponse> content = queryFactory.select(
+                        new QCommentBoardResponse(
+                                comment.id,
+                                comment.writer,
+                                comment.contents,
+                                comment.childs.size()
+                        )
+                )
+                .from(comment)
+                .where(
+                        comment.parent.isNull(),
+                        comment.board.id.eq(boardId)
+                )
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize() + 1)
                 .orderBy(comment.id.desc())
