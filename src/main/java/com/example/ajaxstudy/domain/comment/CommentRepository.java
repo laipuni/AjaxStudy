2f0d90890ec29bb221fad2ddd4ab975fd4b6fbbf
laipuni <2tgb02023@gmail.com>
2024-07-12 11:46:02 +0900

    perf : delete() 1+N 문제를 자식 댓글 삭제 기능, 부모 댓글 삭제 기능 개발로 성능 개선
    만약 댓글 삭제를 delete()로 했을 경우 jpa의 cascade.remove때문에 2 * (1+N)번 (조회, 삭제) 쿼리를 보내서 성능 저하를 일으킨다. 그래서 직접 jpql로 작성한 delete쿼리로 개선했다. 결론적으로 2 * (1+N)번 쿼리는 자식,부모 댓글 삭제로 총 2번의 쿼리로 줄였다.

--- src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
+++ src/main/java/com/example/ajaxstudy/domain/comment/CommentRepository.java
@@ -1,7 +1,22 @@
 package com.example.ajaxstudy.domain.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment,Long>{
+
+    /**
+     * 자식 객체를 먼저 삭제하고 그 후에 부모를 삭제해야 한다.
+     * @param commentId
+     */
+    @Modifying
+    @Query(value = "delete from Comment c where c.parent_id =:commentId",nativeQuery = true)
+    public void deleteChildAllByCommentId(@Param("commentId")Long commentId);
+
+    @Modifying
+    @Query(value = "delete from Comment c where c.id =:commentId",nativeQuery = true)
+    public void deleteById(@Param("commentId")Long commentId);
 
 }
